6b1bbb46e29c997a091346b506c18dee39d00064
Chris Scott <tildechris@google.com>
2021-07-19 10:03:37 -0400

    Use staging bucket URL for zip data.

--- src/data.ts
+++ src/data.ts
@@ -170,7 +170,8 @@ function _fetchRegionalTrendLines(): Promise<RegionalTrendLine[]> {
 }
 
 export function fetchZipData(geoid):Promise<any> {
-  return fetch('./data/42003.geo.json').then(response => response.json());
+  var baseUrl = "https://storage.googleapis.com/covid19-open-data/covid19-vaccination-search-insights/staging/geo";
+  return fetch(`${baseUrl}/${geoid}.geo.json`).then(response => response.json());
 }
 
 export function fetchRegionalTrendsData(): Promise<
