22a5c0e449e61d907bf2f76a3951c31004e1d18c
Chris Scott <tildechris@google.com>
2021-07-30 10:25:54 -0400

    Add Trends chart margin
    A margin around the chart area prevents width overflow.
    At smaller widths, this overflow can effect the whole page layout,
    e.g. the header width which is specified at 100%.  Overflows cause
    100% width to be greater than the actual viewport.

--- src/TimeSeries.svelte
+++ src/TimeSeries.svelte
@@ -41,6 +41,8 @@
     width: 684,
     height: 276,
   };
+  //https://observablehq.com/@d3/margin-convention?collection=@d3/d3-axis
+  const margin = {top:10, bottom:30, left:0, right:30};
 
   function getLegendComponentText(): string {
     if (selectedRegion.sub_region_3) {
@@ -103,6 +105,11 @@
         .attr("class", "chart-legend-text")
         .text(legendComponentText);
     }
+    d3.select(legendContainerElement)
+      .append("div")
+      .attr("class", "chart-legend-text-container chart-axis-label")
+      .append("text")
+      .text("INTEREST");
   }
 
   function generateChartHoverCard(
@@ -458,8 +465,7 @@
     } else {
       x = chartArea
         .append("g")
-        .attr("class", "x axis")
-        .attr("transform", `translate(0, ${chartBounds.height})`);
+        .attr("class", "x axis");
     }
 
     if (yElement) {
@@ -534,10 +540,10 @@
         }
       });
     });
-
+    
     let xScale = d3
       .scaleTime()
-      .range([0, chartBounds.width])
+      .range([margin.left, chartBounds.width-margin.right])
       .domain(d3.extent(dates));
     let xAxis: d3.Axis<Date | d3.NumberValue> = d3
       .axisBottom(xScale)
@@ -550,12 +556,15 @@
 
     let yScale = d3
       .scaleLinear()
-      .range([chartBounds.height, 0])
+      .range([chartBounds.height-margin.bottom, margin.top])
       .domain([0, max]);
-    let yAxis = d3.axisRight(yScale).ticks(5).tickSize(chartBounds.width);
+    let yAxis = d3
+      .axisRight(yScale)
+      .ticks(5).tickSize(chartBounds.width-margin.right);
 
     // TODO(patankar): Define constants for styling.
     x.call(xAxis)
+      .attr("transform",`translate(0,${chartBounds.height-margin.bottom})`)
       .call((g) =>
         g.select(".domain").attr("stroke-width", 1).attr("stroke", "#80868B")
       )
@@ -573,15 +582,8 @@
           .attr("font-size", 12)
       );
     y.call(yAxis)
+    //.attr("transform",`translate(${chartBounds.width-margin.right},0)`)
       .call((g) => g.select(".domain").remove())
-      .call((g) =>
-        g
-          .selectAll(".tick")
-          .filter((t) => {
-            return t === 0;
-          })
-          .remove()
-      )
       .call((g) =>
         g
           .selectAll(".tick line")
@@ -594,18 +596,6 @@
           .attr("color", "#5F6368")
           .attr("font-family", "Roboto")
           .attr("font-size", 12)
-      )
-      .call((g) => g.select(".y-label").remove())
-      .call((g) =>
-        g
-          .append("text")
-          .attr("class", "y-label")
-          .attr("x", chartBounds.width - 15)
-          .attr("y", -10)
-          .attr("fill", "#5F6368")
-          .attr("font-family", "Roboto")
-          .attr("font-size", 11)
-          .text("INTEREST")
       );
 
     scaleChartText();
@@ -656,29 +646,23 @@
       chartXAxisElement.querySelectorAll(".tick text");
     const chartYAxisTickTextElements: NodeList =
       chartYAxisElement.querySelectorAll(".tick text");
-    const chartYAxisLabelElement: SVGElement =
-      chartYAxisElement.querySelector(".y-label");
+
     const chartXAxisTickTexts: SvgSelection = d3.selectAll(
       chartXAxisTickTextElements
     );
     const chartYAxisTickTexts: SvgSelection = d3.selectAll(
       chartYAxisTickTextElements
     );
-    const chartYAxisLabelText: SvgSelection = d3.select(chartYAxisLabelElement);
 
     const chartAreaScale: number =
       chartAreaContainerElement.getBoundingClientRect().width /
       chartBounds.width;
-
     chartXAxisTickTexts
       .attr("transform", `scale(${1 / chartAreaScale})`)
       .attr("y", `${tickSize * chartAreaScale + padding}`);
     chartYAxisTickTexts
       .attr("transform", `scale(${1 / chartAreaScale})`)
-      .attr("x", `${chartBounds.width * chartAreaScale + padding}`);
-    chartYAxisLabelText
-      .attr("transform", `scale(${1 / chartAreaScale})`)
-      .attr("x", `${chartBounds.width * chartAreaScale - 15}`);
+      .attr("x", `${(chartBounds.width-margin.right) * chartAreaScale + padding}`);
   }
 
   onMount(async () => {
