349ae716d5ae36c5cf0045971cb08d83c94c57a4
Jelena Kolomijec <jelenako@google.com>
2022-02-09 18:41:31 +0000

    Initial AU skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -164,6 +164,14 @@
       </p>
       <h4 class="header-download-popup-subtitle">Download dataset</h4>
       <p class="header-download-popup-link-list">
+        <a
+          class="header-download-popup-link"
+          href="https://storage.googleapis.com/covid19-open-data/covid19-vaccination-search-insights/AU_vaccination_search_insights.csv"
+          on:click={(e) => closeDownloadPopup()}
+          ><span class="material-icons-outlined header-download-popup-icon"
+            >file_download</span
+          >Australia</a
+        >
         <!-- <a
           class="header-download-popup-link"
           href="https://storage.googleapis.com/covid19-open-data/covid19-vaccination-search-insights/CA_vaccination_search_insights.csv"
