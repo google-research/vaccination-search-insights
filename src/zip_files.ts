570974fe5644a00b34db701c5c393180ee4e8c35
Steve Herskovits <iamsteve@google.com>
2022-03-01 08:10:31 -0500

    modified zip_fils.ts to use GCS for zips file instead of local version

--- src/zip_files.ts
+++ src/zip_files.ts
@@ -19,11 +19,11 @@ export function fetchZipTrendLines(): Promise<RegionalTrendLine[]> {
   } else {//if (selectedCountryMetadata) {
     let results: Promise<RegionalTrendLine[]> = new Promise(
       (resolve, reject) => {
-        parse("./data/" + "zips_US.csv", {
+        parse("https://storage.googleapis.com/covid19-open-data/covid19-vaccination-search-insights/staging/iamsteve_test/zips_US.csv", { //"./data/" + "zips_US.csv", {
           download: true,
           header: true,
           skipEmptyLines: true,
-          complete: function (results: ParseResult<RegionalTrendLine>) {
+          complete: function (results: ParseResult<RegionalTrendLine>) {, {}
             console.log(
               `Load regional zips trend data with ${results.data.length} from zips file `//${selectedCountryMetadata.dataFile}`
             );
